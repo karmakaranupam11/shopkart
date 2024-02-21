3a7c066b00dab57ac672770b1229190f695b8607
karmakaranupam11 <karmakaranupam11@gmail.com>
2024-02-22 00:19:09 +0530

    Adding Author

--- src/main/java/com/shopkart/ecommerce/ServletInitializer.java
+++ src/main/java/com/shopkart/ecommerce/ServletInitializer.java
@@ -3,6 +3,7 @@ package com.shopkart.ecommerce;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
+// @author Akarmakar
 public class ServletInitializer extends SpringBootServletInitializer {
 
 	@Override
